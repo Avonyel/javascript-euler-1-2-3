02bab30155dc448cb80a194b9f2f98541dcc547b
Stephanie Barker <song_mage@yahoo.com>
2014-05-22 18:23:22 -0400

    Vastly improved primefactor.js efficiency, corrected case where numberToFactor is prime

--- primefactor.js
+++ primefactor.js
@@ -1,13 +1,15 @@
 var biggestPrimeFactor = 1;
-var numberToFactor = 98210069;
-//This inefficient code starts to slow significantly after 8 digits//
+var numberToFactor = 600851475143;
+var factorArray = []
 
-//Figure out if each number is a factor//
-for (i = 0; i < numberToFactor; i++) {
+//Figure out if each number is a factor, up to the numbertoFactor's//
+//square root//
+for (i = 2; i <= Math.sqrt(numberToFactor); i++) {
     if(numberToFactor % i === 0) {
         //If it is a factor, figure out if it's prime//
+        factorArray.push(i);
         var isPrime = true;
-        for(j = 2; j < i; j++) {
+        for(j = 2; j <= Math.sqrt(i); j++) {
             if(i % j === 0) {
                 var isPrime = false;
             }
@@ -16,6 +18,26 @@ for (i = 0; i < numberToFactor; i++) {
             biggestPrimeFactor = i;
         }
     }
+}
+
+//Find the pair for each of the below-the-square-root factors, giving//
+//us the rest of the factors for the numberToFactor, and figure out//
+//if each is prime//
+for (factor in factorArray) {
+    var bigFactor = numberToFactor / factorArray[factor];
+    var isPrime = true
+    for (i = 2; i <= Math.sqrt(bigFactor); i++)
+        if(bigFactor % i === 0) {
+            var isPrime = false;
+        }
+    if(isPrime === true) {
+        biggestPrimeFactor = bigFactor;
+    }
+}
+
+//If no prime factors were found, the numberToFactor must be prime//
+if(biggestPrimeFactor === 1) {
+    biggestPrimeFactor = numberToFactor;
 }
 
 console.log(biggestPrimeFactor);
